1c0bd260f84346e8f6167ca49265e25ee51cb9e4
watts <watts@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-07-01 22:42:18 +0000

    Fixed spelling error

--- wg_invent_client/src/wg_invent_client/invent_client.py
+++ wg_invent_client/src/wg_invent_client/invent_client.py
@@ -32,7 +32,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-##\author Scott Hassen
+##\author Scott Hassan
 ##\brief Client for WG inventory system
 
 
