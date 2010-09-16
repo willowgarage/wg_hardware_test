f964b976defdd08087feb91d9858c9138780aa75
watts <watts@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-09-16 18:06:55 +0000

    Removed bad attribute write to ROS service

--- qualification/tests/wge100_camera_test/shutdown.py
+++ qualification/tests/wge100_camera_test/shutdown.py
@@ -46,6 +46,5 @@ rospy.wait_for_service('shutdown_done', 3)
 done_proxy = rospy.ServiceProxy('shutdown_done', ScriptDone)
 done  = ScriptDoneRequest()
 done.result = ScriptDoneRequest.RESULT_OK
-done.script = 'shutdown.launch'
 done.failure_msg = ''
 done_proxy.call(done)
