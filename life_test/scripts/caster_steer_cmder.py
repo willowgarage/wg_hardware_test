727bea5ad5163c8fa01ba31c224699c20da3e34d
watts <watts@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-12-08 18:35:07 +0000

    Caster steering now actually steers caster

--- life_test/scripts/caster_steer_cmder.py
+++ life_test/scripts/caster_steer_cmder.py
@@ -42,7 +42,7 @@ TOPIC_PREFIX = 'caster_fl'
 
 # CHANGE THIS NUMBER TO CHANGE THE STEER VELOCITY OF THE CASTER
 STEER_VEL = 6.0 
-SPEED = 3.7
+SPEED = 0.0
 
                     
 def main():
@@ -54,8 +54,8 @@ def main():
 
     my_rate = rospy.Rate(float(rospy.get_param('cycle_rate', 1.0)))
     while not rospy.is_shutdown():
-        pub_steer.publish(Float64(0.0))
-        #pub_steer.publish(Float64(STEER_VEL))
+        #pub_steer.publish(Float64(0.0))
+        pub_steer.publish(Float64(STEER_VEL))
         pub_drive.publish(Float64(SPEED))
 
         my_rate.sleep()
